a23cc01048b0738624ecad97856f585f44df2eaf
DESKTOP-9044RHE\admin <yaoxiuyi888@gmail.com>
2025-12-09 14:44:58 +0800

    no message

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import "./styles/globals.css";
 import "./index.css";
 // import "./index1.css";
 import { setupAxiosInterceptors } from './api/axiosInterceptors';
+import { initOpenAPI } from './api/setup';
 
 setupAxiosInterceptors();
 
 import { QueryProvider } from "./providers/QueryProvider";
 
-createRoot(document.getElementById("root")!).render(
-    <QueryProvider>
-        <App />
-    </QueryProvider>
-);
+initOpenAPI().then(() => {
+    createRoot(document.getElementById("root")!).render(
+        <QueryProvider>
+            <App />
+        </QueryProvider>
+    );
+});
